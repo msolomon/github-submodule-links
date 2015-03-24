d9ef100a4f5265c02be8ae788a4f49d3f2fb1ca1
Allan Mørk Christensen <allanmc@gmail.com>
2015-03-24 14:08:59 +0100

    Complete overhaul
    * Insert aciton links, instead of rewriting commit refs as links
    * Better AJAX waiting
    * Removed lookup of unrelated submodules in .gitmodules
    * Introduced simple caching of .gitmodules

--- link_maker.js
+++ link_maker.js
@@ -1,105 +1,178 @@
-// This code is pretty hacky and very much tied to GitHub's current HTML.
-// Expect it to break when GitHub changes things around, or just at random.
 
-function rewriteSubmoduleHashesToLinks() {
-    var containingElement = document.querySelectorAll("td.blob-code");
-    var submoduleHashElements = [];
+function addSubmoduleDiffLinks() {
+    var containingElement = document.querySelectorAll("div#diff div.file, div#files div.file");
     var submoduleHashes = [];
 
     for (var i = 0; i < containingElement.length; i++) {
-        var processedHTML = containingElement[i].innerHTML;
-        processedHTML = processedHTML.replace(/<[^>]*>/g, '');
-        var trymatch = processedHTML.match(/Subproject commit ([a-f0-9]{40})/);
-        if (!trymatch) continue;
-        processedHTML = trymatch[1];
-        if (/[a-f0-9]{40}/.test(processedHTML)) {
-            submoduleHashElements.push(containingElement[i])
-            submoduleHashes.push(processedHTML);
-        }
-    }
-
-    if (submoduleHashElements.length > 0) {
-        fetchSubmodulePaths(function(submodulePaths) {
-            for (var i = 0; i < submoduleHashElements.length; i++) {
-                rewriteSubmoduleHashToLink(submoduleHashElements[i], submoduleHashes[i], submodulePaths, submoduleHashes);
-            }
-        });
+		var element = containingElement[i];
+		
+		var header = element.querySelectorAll("div.file-header");
+		if (header.length != 1) {
+			// Could not find file-header.
+			continue;
+		}
+		header = header[0];
+		var filePath = header.getAttribute('data-path');
+		
+		var codeBlobs = element.querySelectorAll("td.blob-code-deletion, td.blob-code-addition");
+	
+		if (codeBlobs.length < 1 || codeBlobs.length > 2) {
+			// Submodule blobs always contains one or two code blobs (add and/or remove).
+			continue;
+		}
+	
+		var firstCommit = codeBlobs[0].innerHTML.replace(/<[^>]*>/g, '');
+		var secondCommit;
+		if (codeBlobs[1]) {
+			secondCommit = codeBlobs[1].innerHTML.replace(/<[^>]*>/g, '');
+		}
+	
+        var matchFirstCommit = firstCommit.match(/\Subproject commit ([a-f0-9]{40})/);
+		var matchSecondCommit;
+		if (secondCommit) {
+			matchSecondCommit = secondCommit.match(/\Subproject commit ([a-f0-9]{40})/);
+		}
+	
+        if (!matchFirstCommit || (secondCommit && !matchSecondCommit)) {
+			// Blob was just subproject related
+			continue;
+		}
+		
+		firstCommit = matchFirstCommit[1];
+		if (secondCommit) {
+			secondCommit = matchSecondCommit[1];
+		}
+		
+		var fileActionsElm = header.querySelectorAll("div.file-actions");
+		if (fileActionsElm.length != 1) {
+			// Header-actions was not found.
+			continue;
+		}
+		
+		fileActionsElm = fileActionsElm[0];
+		
+		fetchSubmodulePaths(filePath, function(filePath, firstCommit, secondCommit, fileActionsElm, submodulePath) {
+			if (submodulePath) {
+				var diffUrl = 'https://github.com/' + submodulePath + '/compare/' + firstCommit + '...' + secondCommit;
+				var firstCommitUrl = 'https://github.com/' + submodulePath + '/tree/' + firstCommit;
+				var secondCommitUrl = 'https://github.com/' + submodulePath + '/tree/' + secondCommit;
+				var diffMenu = fileActionsElm.querySelectorAll('div.submodule-diff');
+				var classes = 'class="btn btn-sm tooltipped tooltipped-n"';
+				
+				var html ='';
+				if (secondCommit) {
+					html += '<a href="'+diffUrl+'" '+classes+' aria-label="View the diff between the old and the new submodule commit">Diff '+firstCommit.substring(0, 4)+'...'+secondCommit.substring(0, 4)+'</a> ';
+				}
+				html += '<a href="'+firstCommitUrl+'" '+classes+' aria-label="View the submodule commit">'+firstCommit.substring(0, 4)+'</a> ';
+				if (secondCommit) {
+					html += '<a href="'+secondCommitUrl+'" '+classes+' aria-label="View the submodule commit">'+secondCommit.substring(0, 4)+'</a>';
+				}
+			
+				// Did we already add the menu?
+				if (diffMenu.length == 0) {
+					var div = document.createElement('div');
+					div.className = "submodule-diff";
+					div.innerHTML = html;
+					fileActionsElm.insertBefore( div, fileActionsElm.childNodes[0] );
+				} else {
+					diffMenu[0].innerHTML = html;
+				}
+			} else {
+				console.log("Could not find submodule: " + filePath);
+			}
+		}.bind(undefined, filePath, firstCommit, secondCommit, fileActionsElm));
+		
+		/*
+		fetchSubmodulePaths(filePath, function(firstCommit, secondCommit, fileActionsElm) {
+			return function(submodulePath) {
+				if (submodulePath) {
+					console.log('submodulePath: ' + submodulePath);
+					var diffUrl = 'https://github.com/' + submodulePath + '/compare/' + firstCommit + '...' + secondCommit;
+					var firstCommitUrl = 'https://github.com/' + submodulePath + '/tree/' + firstCommit;
+					var secondCommitUrl = 'https://github.com/' + submodulePath + '/tree/' + secondCommit;
+					var diffMenu = fileActionsElm.querySelectorAll('div.submodule-diff');
+					var classes = 'class="btn btn-sm tooltipped tooltipped-n"';
+					
+					var html ='';
+					if (secondCommit) {
+						html += '<a href="'+diffUrl+'" '+classes+' aria-label="View the diff between the old and the new submodule commit">Diff '+firstCommit.substring(0, 4)+'...'+secondCommit.substring(0, 4)+'</a> ';
+					}
+					html += '<a href="'+firstCommitUrl+'" '+classes+' aria-label="View the submodule commit">'+firstCommit.substring(0, 4)+'</a> ';
+					if (secondCommit) {
+						html += '<a href="'+secondCommitUrl+'" '+classes+' aria-label="View the submodule commit">'+secondCommit.substring(0, 4)+'</a>';
+					}
+				
+					// Did we already add the menu?
+					if (diffMenu.length == 0) {
+						var div = document.createElement('div');
+						div.className = "submodule-diff";
+						div.innerHTML = html;
+						fileActionsElm.insertBefore( div, fileActionsElm.childNodes[0] );
+					} else {
+						diffMenu[0].innerHTML = html;
+					}
+				} else {
+					console.log("Could not find submodule.");
+				}
+			};
+		}(firstCommit, secondCommit, fileActionsElm));
+		*/
     }
 }
 
-function fetchSubmodulePaths(callback) {
+var gitmodulesBlob;
+function fetchSubmodulePaths(filePath, callback) {
     var guessAtCurrentCommitForGitConfig = document.body.innerHTML.match(/commit\/([a-f0-9]{40})/)[1];
 
     var currentRepoPath = window.location.pathname.match(/\/[^\/]*\/[^\/]*\//)[0];
 
     var gitModulesPath = 'https://github.com' + currentRepoPath + 'blob/' + guessAtCurrentCommitForGitConfig + '/.gitmodules'
-
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        var submoduleRegexes = [/url\s*=[^>]*github.com:([^<\s]*)\.git/g, /url\s*=[^>]*git:\/\/github.com\/([^<\s]*).git/g, /url\s*=[^>]*https?:\/\/github.com\/([^<\s]*).git/g];
-        var submodules = [];
-        for (var i = 0; i < submoduleRegexes.length; i++) {
-            while ((submodule = submoduleRegexes[i].exec(xhr.responseText)) !== null) {
-                submodules.push(submodule[1]);
-            }
-        }
-
-        callback(submodules);
-    }
-    xhr.open("GET", gitModulesPath);
-    xhr.send();
+	
+	if (gitmodulesBlob) {
+		return findSubmodulePath(filePath, gitmodulesBlob, callback);
+	} else {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function() {
+			gitmodulesBlob = xhr.responseText.match(/<div class="blob-wrapper data type-text">(\s|.)*?<\/div>/);
+			if (!gitmodulesBlob) {
+				console.log("Could not find .gitmodules content.");
+				return callback();
+			}
+			// Remove HTML tags.
+			gitmodulesBlob = gitmodulesBlob[0].replace(/<[^>]*>/g, '');
+		
+			return findSubmodulePath(filePath, gitmodulesBlob, callback);
+		}
+		xhr.open("GET", gitModulesPath);
+		xhr.send();
+	}
 }
 
-function rewriteSubmoduleHashToLink(submoduleHashElement, submoduleHash, submodulePaths, submoduleHashes) {
-    for (var i = 0; i < submodulePaths.length; i++) {
-        var submoduleCommitPath = 'https://github.com/' + submodulePaths[i] + '/commit/' + submoduleHash;
-        rewriteLinkIfValid(submoduleCommitPath, submoduleHashElement, submoduleHash, submoduleHashes);
-    }
+function findSubmodulePath(filePath, gitmodulesBlob, callback) {
+	// Only look at the section related to the requested submodule
+	var submodulePart = gitmodulesBlob.match(new RegExp('\\[submodule[^\\]]+'+filePath+'[^\\]]*\][^\\[]*'));
+	
+	if (!submodulePart) {
+		console.log("Could not find correct submodule in .gitmodules.");
+		return callback();
+	}
+	submodulePart = submodulePart[0];
+	
+	var submoduleRegexes = [/url\s*=[^>]*github\.com:([^<\s]*)\.git/, /url\s*=[^>]*git:\/\/github\.com\/([^<\s]*)\.git/, /url\s*=[^>]*https?:\/\/github\.com\/([^\s]+?)(\.git)?(\s|$)/];
+	var submodules = [];
+	var submodule;
+	for (var i = 0; i < submoduleRegexes.length; i++) {
+		submodule = submodulePart.match(submoduleRegexes[i]);
+		if (submodule) {
+			return callback(submodule[1]);
+		}
+	}
+	console.log("Could not find matching github submodule url in blob.");
+	return callback();
 }
-
-function rewriteLinkIfValid(submoduleCommitPath, submoduleHashElement, submoduleHash, submoduleHashes) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            submoduleHashElement.innerHTML = '<a href="' + submoduleCommitPath + '">' +
-                    submoduleHashElement.innerHTML +
-                '</a>' +  getDiffLinkPlaceholderHTMLAndInsertIfExists(submoduleCommitPath, submoduleHash, submoduleHashes);
-                ;
-        }
-    }
-    xhr.open("HEAD", submoduleCommitPath);
-    xhr.send();
-}
-
-function getDiffLinkPlaceholderHTMLAndInsertIfExists(submoduleCommitPath, submoduleHash, submoduleHashes) {
-    var placeholderHTML = "";
-    for (var i = 0; i < submoduleHashes.length; i++) {
-        if (submoduleHash == submoduleHashes[i]) continue;
-        placeholderHTML += '<span id="' + submoduleHash + '…' + submoduleHashes[i] + '"></span>';
-        insertDiffLinkIfValid(submoduleCommitPath, submoduleHash, submoduleHashes[i]);
-    }
-
-    return placeholderHTML;
-}
-
-function insertDiffLinkIfValid(submoduleCommitPath, submoduleHash, nextSubmoduleHash) {
-    var diffURL = submoduleCommitPath.replace(/\/commit\//, '/compare/') + '...' + nextSubmoduleHash;
-    var shortDiff = submoduleHash.substring(0, 4) + '…' + nextSubmoduleHash.substring(0, 4);
-
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            var placeholder = document.getElementById(submoduleHash + '…' + nextSubmoduleHash);
-            placeholder.innerHTML = ' <a href="' + diffURL + '">' + shortDiff + '</a> ';
-        }
-    }
-    xhr.open("HEAD", diffURL);
-    xhr.send();
-}
-
 
 // Run on page load
-rewriteSubmoduleHashesToLinks();
+addSubmoduleDiffLinks();
 
 // Run every second in case the page was partially swapped in as GitHub loves to do
 // TODO: find an event that reliably fires on soft page loads (URL change maybe? can't find an event for that)
@@ -107,6 +180,9 @@ var lastUrl = window.location.href;
 setInterval(function() {
     if (window.location.href != lastUrl) {
         lastUrl = window.location.href;
-        rewriteSubmoduleHashesToLinks();
+		// If the URL changed because of an AJAX request, we need to wait some time for the request to complete.
+		setTimeout(function  () {
+			addSubmoduleDiffLinks();
+		}, 500);
     }
-}, 1000)
+}, 500);
